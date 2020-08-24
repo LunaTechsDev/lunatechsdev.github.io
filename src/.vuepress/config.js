b9f6ecf6e88cd9bcc8b490b4bdfdeed0891d17e2
waynee95 <larsericmarquardt@mail.de>
2020-08-24 10:55:03 +0200

    Add favicon

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -4,7 +4,37 @@ module.exports = resolve({
   title: "LunaTechsDev",
   description: "Just playing around",
   baseLang: "en-US",
-  head: [["script", { src: "https://hcaptcha.com/1/api.js" }, "async defer"]],
+  head: [
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
+    ],
+    ["link", { rel: "manifest", href: "/site.webmanifest" }],
+    ["link", { rel: "shortcut icon", href: "/favicon.ico" }],
+    ["script", { src: "https://hcaptcha.com/1/api.js" }, "async defer"],
+  ],
   locales: {
     "/": {
       lang: "en-US",
